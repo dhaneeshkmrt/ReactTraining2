f6d3be23f9be8ea3a2ec04cd7330d28af1a98c06
Dhaneesh Thulasidharan <Dhaneesh.Thulasdharan@refinitiv.com>
2020-08-21 11:16:15 +0530

    error boundry fix

--- src/invision/components/error-boundary/error-boundary.js
+++ src/invision/components/error-boundary/error-boundary.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default class ErrorBoundary extends React.Component {
-  constructor() {
+  constructor(props) {
+    super(props)
     this.state = { hasError: false };
   }
 
