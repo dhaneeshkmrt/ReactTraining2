34491756ae31abcd4f01e35f4e6ab63c7a1a2e1d
Dhaneesh Thulasidharan <Dhaneesh.Thulasdharan@refinitiv.com>
2020-10-01 16:03:43 +0530

    forms completed

--- src/invision/components/movie-popup/movie.popup.js
+++ src/invision/components/movie-popup/movie.popup.js
@@ -1,43 +1,68 @@
 import React, { useState } from "react";
 import Modal from 'react-awesome-modal';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default function MoviePopup(props) {
 
 
   const isAdd = props.title === 'Add Movie';
 
-  const [movie, setState] = useState({ ...props.movie });
+  // const [movie, setState] = useState({ ...props.movie });
+  const movie = {...props.movie};
+  let releasedDate = movie.releasedDate;
+  if (releasedDate) {
+    const date = new Date(releasedDate)
+    movie.releasedDate = `${date.getFullYear()}-${date.getMonth().toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}`;
+  }
+  const initialState = movie;
+  const validate = (values) => {
+    const errors = {};
 
-  const reset = () => {
-    setState({
-      ...props.movie,
-    });
+    if (!values.title.trim()) {
+      errors.title = 'required';
+    }
+
+    if (!values.releasedDate) {
+      errors.releasedDate = 'required';
+    }
+
+    const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+(?:png|jpg|jpeg|gif|svg)+$/g;
+    if (!values.thumbnail) {
+      errors.thumbnail = 'required';
+    } else if (!urlRegex.test(values.thumbnail)) {
+      errors.thumbnail = 'Url format expected';
+    }
+
+    if (!values.genre.trim()) {
+      errors.genre = 'required';
+    }
+
+    if (!values.overview) {
+      errors.overview = 'required';
+    } else if (values.overview.length < 5) {
+      errors.overview = 'Overview should be more than 5 chars';
+    }
+
+    if (!values.runTime) {
+      errors.runTime = 'required';
+    } else if (!isFloat(parseFloat(values.runTime))) {
+      errors.runTime = 'Runtime should be a number';
+    }
+
+    if (!values.rating) {
+      errors.rating = 'required';
+    } else if (!isFloat(parseFloat(values.rating))) {
+      errors.rating = 'Rating should be a number';
+    }
+    return errors;
   }
 
-  const handleTitleChange = (event) => {
-    setState({ ...movie, title: event.target.value });
+  function isFloat(n) {
+    return Number(n) === n || n % 1 !== 0;
   }
 
-  const handleReleaseDateChange = (event) => {
-    setState({ ...movie, releasedDate: event.target.value });
-  }
-
-  const handleUrlChange = (event) => {
-    setState({ ...movie, thumbnail: event.target.value });
-  }
-  const handleGenreChange = (event) => {
-    setState({ ...movie, genre: event.target.value });
-  }
-
-  const handleRuntimeChange = (event) => {
-    setState({ ...movie, runTime: event.target.value });
-  }
-
-  const handleOverviewChange = (event) => {
-    setState({ ...movie, overview: event.target.value });
-  }
-  const handleRatingChange = (event) => {
-    setState({ ...movie, rating: event.target.value });
+  const onSubmit = (values, options) => {
+    props.onModalClose({ visible: false, updatedMovie: values, isAdd, isSubmit: true });
   }
 
   return (
@@ -45,43 +70,52 @@ export default function MoviePopup(props) {
       <div className="add-movie-ctnr">
         <div className="header-close-btn"><a onClick={() => props.onModalClose(false)}>&times;</a></div>
         <h1>{props.title}</h1>
-        <form className="form-ctnr">
-          <div className="form-field">
-            <label htmlFor="title">Title</label><br />
-            <input type="text" placeholder="Title" value={movie.title} onChange={handleTitleChange} />
+        <Formik initialValues={initialState} validate={validate} onSubmit={onSubmit}>
+          <Form className="form-ctnr">
+            <div className="form-field">
+              <label htmlFor="title">Title</label><br />
+              <Field type="text" name="title" />
+              <ErrorMessage name="title" component="div" />
+            </div>
             <div className="form-field">
               <label htmlFor="title">Release Date</label><br />
-              <input type="date" placeholder="Release Date" value={movie.releasedDate} onChange={handleReleaseDateChange} />
+              <Field type="date" name="releasedDate" min="1900-1-1" />
+              <ErrorMessage name="releasedDate" component="div" />
             </div>
             <div className="form-field">
               <label htmlFor="title">Movie URL</label><br />
-              <input type="text" placeholder="Movie URL" value={movie.thumbnail} onChange={handleUrlChange} />
+              <Field type="text" name="thumbnail" />
+              <ErrorMessage name="thumbnail" component="div" />
             </div>
             <div className="form-field">
               <label htmlFor="title">GENRE</label><br />
-              <input type="text" placeholder="GENRE" value={movie.genre} onChange={handleGenreChange} />
+              <Field type="text" name="genre" />
+              <ErrorMessage name="genre" component="div" />
             </div>
             <div className="form-field">
               <label htmlFor="title">OVERVIEW</label><br />
-              <input type="text" placeholder="OVERVIEW" value={movie.overview} onChange={handleOverviewChange} />
+              <Field type="text" name="overview" />
+              <ErrorMessage name="overview" component="div" />
             </div>
             <div className="form-field">
               <label htmlFor="title">Rating</label><br />
-              <input type="text" placeholder="OVERVIEW" value={movie.rating} onChange={handleRatingChange} />
+              <Field type="text" name="rating" />
+              <ErrorMessage name="rating" component="div" />
             </div>
             <div className="form-field">
               <label htmlFor="title">RUNTIME</label><br />
-              <input type="text" placeholder="RUNTIME" value={movie.runTime} onChange={handleRuntimeChange} />
+              <Field type="text" name="runTime" />
+              <ErrorMessage name="runTime" component="div" />
             </div>
 
             <div className="form-footer">
-              <button className="primary-btn reset-button" onClick={(e) => { e.preventDefault(); reset() }}>Reset</button>
-              <button className="primary-btn" onClick={(e) => { e.preventDefault(); props.onModalClose({ visible: false, updatedMovie: movie, isAdd, isSubmit: true }) }} >Submit</button>
+              <button className="primary-btn reset-button" type="reset">Reset</button>
+              <button type="submit" className="primary-btn" >Submit</button>
             </div>
-          </div>
-        </form>
+          </Form>
+        </Formik>
       </div>
-    </Modal>
+    </Modal >
   )
 
 }
