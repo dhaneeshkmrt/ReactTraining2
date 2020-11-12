dea96340aa7725c6a10ac8506e2d945a67f8890c
Dhaneesh Thulasidharan <Dhaneesh.Thulasdharan@refinitiv.com>
2020-11-12 12:13:53 +0530

    last change with render error

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-// if (process.env.NODE_ENV === 'development') {
-  const webpack = require('webpack');
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack'); 
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
@@ -14,11 +14,11 @@ const app = express();
   app.use(webpackDevMiddleware(compiler));
   app.use(webpackHotMiddleware(compiler.compilers.find(c => c.name === 'client')));
   app.use(require("webpack-hot-middleware")(compiler));
-// } else {
-//   const serverRenderer = require('../public/js/serverRenderer').default;
+} else {
+  const serverRenderer = require('../public/js/serverRenderer').default;
 
-//   app.use(express.static('public'));
-//   app.use(serverRenderer());
-// }
+  app.use(express.static('public'));
+  app.use(serverRenderer());
+}
 
 module.exports = app;
